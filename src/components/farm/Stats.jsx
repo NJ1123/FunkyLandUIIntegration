cb6b8482bbe4f85addeb22715fab65bbdce00b9a
Yunip Shrestha <yunipshrestha@gmail.com>
2022-07-24 12:43:33 +0545

    Hide total funkies sections in Stats

--- src/components/farm/Stats.jsx
+++ src/components/farm/Stats.jsx
@@ -4,8 +4,8 @@ import { bullAvatar } from "../../assets/images/images";
 function Stats() {
   return (
     <div>
-      <div className="grid grid-cols-4 rounded-tl-xl rounded-tr-xl text-white text-2xl text-center">
-        <div className="stat-box  ">
+      <div className="grid grid-cols-3 md:grid-cols-4 rounded-tl-xl rounded-tr-xl text-white text-2xl text-center">
+        <div className="stat-box hidden md:block ">
           <h1>Total Funkies</h1>
           <p>15</p>
         </div>
