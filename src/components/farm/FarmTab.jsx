a612a3e4fd86c6fbda8881667e55ed38c0cea612
Yunip Shrestha <yunipshrestha@gmail.com>
2022-07-25 21:58:46 +0545

    cards displayed on FarmTab

--- src/components/farm/FarmTab.jsx
+++ src/components/farm/FarmTab.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import config from "../../config/config.json";
 import axios from "axios";
 import BigNumber from "bignumber.js";
+import { convertStringToHex } from "../../utils/convertToHex";
 
 import {
   Address,
@@ -38,30 +39,13 @@ function FarmTab() {
   //   }
   // }, [wallet]);
   const proxy = new ProxyProvider(config.devnet.network.apiAddress);
+  //========== GET STAKING STATE =====================
   const getStakingState = async () => {
     const stakingStateQuery = new Query({
       address: new Address(config.stakingContract),
       func: new ContractFunction("getStakingState"),
     });
     const { returnData } = await proxy.queryContract(stakingStateQuery);
-    return returnData;
-  };
-  const getAddressStake = async () => {
-    const addressStateQuery = new Query({
-      address: new Address(config.stakingContract),
-      func: new ContractFunction("getAddressState"),
-      args: [new AddressValue(new Address(wallet.address))],
-    });
-    const { returnData } = await proxy.queryContract(addressStateQuery);
-    return returnData;
-  };
-  const getNonceStake = async () => {
-    const nonceStateQuery = new Query({
-      address: new Address(config.stakingContract),
-      func: new ContractFunction("getNonceState"),
-      args: [new U64Value("07")],
-    });
-    const { returnData } = await proxy.queryContract(nonceStateQuery);
     return returnData;
   };
   useEffect(() => {
@@ -74,7 +58,72 @@ function FarmTab() {
     };
     getStakingStateAsync();
   }, [wallet]);
+  const splitStakingState = (stakingBuffer) => {
+    let a = 0;
+    const isActive = "01" == stakingBuffer.toString("hex", a, (a += 1));
+    const currentEpoch = parseInt(
+      stakingBuffer.toString("hex", a, (a += 8)),
+      16
+    );
+    const currentStakingRound = parseInt(
+      stakingBuffer.toString("hex", a, (a += 8)),
+      16
+    );
 
+    // const totalStakedAmount = new U64Value(
+    //   "0x" + stakingBuffer.toString("hex", a, (a += m))
+    // ).toNumber();
+
+    const totalStakedAmount = parseInt(
+      stakingBuffer.toString("hex", a, (a += 8)),
+      16
+    );
+    const totalBullStakedAmount = parseInt(
+      stakingBuffer.toString("hex", a, (a += 8)),
+      16
+    );
+    const total_bear_staked_amount = parseInt(
+      stakingBuffer.toString("hex", a, (a += 8)),
+      16
+    );
+    const total_ninja_bear_staked_amount = parseInt(
+      stakingBuffer.toString("hex", a, (a += 8)),
+      16
+    );
+    const total_lone_bear_staked_amount = parseInt(
+      stakingBuffer.toString("hex", a, (a += 8)),
+      16
+    );
+    const total_funky_sluggers_staked_amount = parseInt(
+      stakingBuffer.toString("hex", a, (a += 8)),
+      16
+    );
+
+    return {
+      isActive,
+      currentEpoch,
+      currentStakingRound,
+      totalStakedAmount,
+      totalBullStakedAmount,
+      total_bear_staked_amount,
+      total_ninja_bear_staked_amount,
+      total_lone_bear_staked_amount,
+      total_funky_sluggers_staked_amount,
+    };
+  };
+
+  //========== GET STAKING STATE =====================
+
+  //========== GET ADDRESS STATE =====================
+  const getAddressStake = async () => {
+    const addressStateQuery = new Query({
+      address: new Address(config.stakingContract),
+      func: new ContractFunction("getAddressState"),
+      args: [new AddressValue(new Address(wallet.address))],
+    });
+    const { returnData } = await proxy.queryContract(addressStateQuery);
+    return returnData;
+  };
   useEffect(() => {
     const getAddressStateAsync = async () => {
       if (!wallet.address) {
@@ -85,7 +134,7 @@ function FarmTab() {
       const addressBuffer = Buffer.from(addressStakeData[0], "base64");
       console.log(
         "(splitAddressStake(addressBuffer))",
-        splitAddressStake(addressBuffer)
+        splitAddressStake(addressStakeData)
       );
 
       // const rewardData = await getRewards();
@@ -98,18 +147,77 @@ function FarmTab() {
     };
     getAddressStateAsync();
   }, [wallet]);
+
+  const splitAddressStake = (addressStake) => {
+    let a = 0;
+    let m = addressStake.readUInt32BE(a);
+    a += 4;
+    const barrel_capacity =
+      new BigNumber(
+        "0x" + addressStake.toString("hex", a, (a += m))
+      ).toNumber() /
+      10 ** 5;
+
+    /////////////////////////
+    let n = addressStake.readUInt32BE(a);
+    a += 4;
+    const rewards_collected =
+      new BigNumber(
+        "0x" + addressStake.toString("hex", a, (a += n))
+      ).toNumber() /
+      10 ** 5;
+    ///////////////////////////////
+    return {
+      barrel_capacity,
+      rewards_collected,
+    };
+  };
+  //========== GET ADDRESS STATE =====================
+
+  //========== GET NONCE STATE =====================
+
+  const splitNonceStake = (NonceStake) => {
+    let a = 0;
+    const index = parseInt(NonceStake.toString("hex", a, (a += 4)), 16);
+    const staked = "01" == NonceStake.toString("hex", a, (a += 1));
+    const unlock_epoch = parseInt(NonceStake.toString("hex", a, (a += 8)), 16);
+    const current_epoch = parseInt(NonceStake.toString("hex", a, (a += 8)), 16);
+    const lastclaimedround = parseInt(
+      NonceStake.toString("hex", a, (a += 8)),
+      16
+    );
+
+    return {
+      index,
+      staked,
+      unlock_epoch,
+      current_epoch,
+      lastclaimedround,
+    };
+  };
+
+  const getNonceStake = async () => {
+    const nonceStateQuery = new Query({
+      address: new Address(config.stakingContract),
+      func: new ContractFunction("getNonceState"),
+      args: [new U64Value("07")],
+    });
+    const { returnData } = await proxy.queryContract(nonceStateQuery);
+    return returnData;
+  };
+
   useEffect(() => {
     const getNonceStateAsync = async () => {
       if (!wallet.address) {
         return;
       }
       const nonceStakeData = await getNonceStake();
-      console.log("nonceStakeData", nonceStakeData);
+      // console.log("nonceStakeData", nonceStakeData);
       const nonceBuffer = Buffer.from(nonceStakeData[0], "base64");
-      console.log(
-        "(splitNonceStake(nonceBuffer))",
-        splitNonceStake(nonceBuffer)
-      );
+      // console.log(
+      //   "(splitNonceStake(nonceBuffer))",
+      //   splitNonceStake(nonceBuffer)
+      // );
 
       // const rewardData = await getRewards();
       // setReward(
@@ -122,102 +230,38 @@ function FarmTab() {
     getNonceStateAsync();
   }, [wallet]);
 
-  const splitStakingState = (stakingBuffer) => {
-    let a = 0;
-    const isActive = "01" == stakingBuffer.toString("hex", a, (a += 1));
-    const currentEpoch = parseInt(
-      stakingBuffer.toString("hex", a, (a += 8)),
-      16
-    );
-    const currentStakingRound = parseInt(
-      stakingBuffer.toString("hex", a, (a += 8)),
-      16
-    );
-    
-    // const totalStakedAmount = new U64Value(
-    //   "0x" + stakingBuffer.toString("hex", a, (a += m))
-    // ).toNumber();
+  //========== GET NONCE STATE =====================
 
-    const totalStakedAmount = parseInt(
-      + stakingBuffer.toString("hex", a, (a += 8)),16
-    );
-    const totalBullStakedAmount = parseInt(
-      + stakingBuffer.toString("hex", a, (a += 8))
-    );
-    const total_bear_staked_amount= parseInt(
-      + stakingBuffer.toString("hex", a, (a += 8))
-    );
-    const total_ninja_bear_staked_amount= parseInt(
-      + stakingBuffer.toString("hex", a, (a += 8))
-    );
-    const total_lone_bear_staked_amount=parseInt(
-      + stakingBuffer.toString("hex", a, (a += 8))
-    );
-    const total_funky_sluggers_staked_amount=parseInt(
-      + stakingBuffer.toString("hex", a, (a += 8))
-    );
+  //====== GET USERS STAKED TOKENS STATE =================
 
-    return {
-      isActive,
-      currentEpoch,
-      currentStakingRound,
-      totalStakedAmount,
-      totalBullStakedAmount,
-      total_bear_staked_amount,
-      total_ninja_bear_staked_amount,
-      total_lone_bear_staked_amount,
-      total_funky_sluggers_staked_amount
-
-    };
+  const getUsersStakedTokensState = async () => {
+    const usersStakedTokensStateQuery = new Query({
+      address: new Address(config.stakingContract),
+      func: new ContractFunction("users_staked_tokens"),
+      args: [new AddressValue(new Address(wallet.address))],
+    });
+    const { returnData } = await proxy.queryContract(
+      usersStakedTokensStateQuery
+    );
+    return returnData;
   };
-
-  const splitAddressStake = (addressStake) => {
-    let a = 0;
-    const barrel_capacity  = parseInt(addressStake.toString("hex", a, (a += 8)), 16);
- 
-/////////////////////////
-    let m = addressStake.readUInt32BE(a)
-    a += 4
-    const rewards_collected =
-      new BigNumber(
-        "0x" + addressStake.toString("hex", a, (a += m)),
-      ).toNumber() /
-        10 ** 5 || 0
-///////////////////////////////
-    return {
-      barrel_capacity,
-      rewards_collected,
-
-    };
-  };
-
-  const splitNonceStake = (NonceStake) => {
-    let a = 0;
-    const index  = parseInt(NonceStake.toString("hex", a, (a += 4)), 16);
-    const staked = "01" == NonceStake.toString("hex", a, (a += 1));
-    const unlock_epoch =parseInt(
-      NonceStake.toString("hex", a, (a += 8)),
-      16
-    );
-      const current_epoch = parseInt(
-        NonceStake.toString("hex", a, (a += 8)),
-        16
+  useEffect(() => {
+    const getUsersStakedTokensStateAsync = async () => {
+      let a = 0;
+      const usersStakedTokensStates = await getUsersStakedTokensState();
+      const usersStakedTokensBuffers = usersStakedTokensStates.map(
+        (usersStakedTokensState) =>
+          Buffer.from(usersStakedTokensState, "base64").toString("hex", a)
       );
-    const lastclaimedround=parseInt(
-      NonceStake.toString("hex", a, (a += 8)),
-      16
-    );
- 
-
-    return {
-      index,
-      staked,
-      unlock_epoch,
-      current_epoch,
-      lastclaimedround
-
+      setUserNfts(usersStakedTokensBuffers);
+      console.log("usersStakedTokensState", usersStakedTokensStates);
+      console.log("usersStakedTokensBuffer", usersStakedTokensBuffers);
     };
-  };
+    getUsersStakedTokensStateAsync();
+  }, [wallet]);
+
+  //====== GET USERS STAKED TOKENS STATE =================
+
   return (
     <div>
       <div>
@@ -226,7 +270,11 @@ function FarmTab() {
           <ClaimAmount />
           <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-10 mt-10">
             {userNfts.map((userNft) => (
-              <FunkyCard type="farm" content={userNft} key={userNft.nonce} />
+              <FunkyCard
+                type="farm"
+                content={{ nonce: 1, name: "Radhe Radhe" }}
+                key={1}
+              />
             ))}
           </div>
         </div>
